146658c007405d0130a9a4687b66e1712be383ef
Lilian Bernot <lilian.bernot@gmail.com>
2023-05-27 10:38:27 -0400

    the form stores the information. Changing the .gitignore : does not add the sqll data

--- bsport-technical-assessment-backend-main/family/views.py
+++ bsport-technical-assessment-backend-main/family/views.py
@@ -4,15 +4,15 @@ from .forms import FamilyForm
 
 # Create your views here.
 def family_list(request):
-    # families = Family.objects.all()
-    # return render(request, 'family/family_list.html', {'families': families})
-    return render(request, 'family/family_list.html', {'families': [0, 1, 2]})
+    families = Family.objects.all()
+    return render(request, 'family/family_list.html', {'families': families})
+    # return render(request, 'family/family_list.html', {'families': [0, 1, 2]})
 
 def family_create(request):
     if request.method == 'POST':
         form = FamilyForm(request.POST)
         if form.is_valid():
-            # form.save()
+            form.save()
             return redirect('family:family_list')
     else:
         form = FamilyForm()
