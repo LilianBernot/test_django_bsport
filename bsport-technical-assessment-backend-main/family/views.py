3c77f7674854a1f4f6fa15194064fb638e3e40dd
Lilian Bernot <lilian.bernot@gmail.com>
2023-05-27 10:12:05 -0400

    creation of a form. It does not store, we still have to create the database

--- bsport-technical-assessment-backend-main/family/views.py
+++ bsport-technical-assessment-backend-main/family/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Family
+from .forms import FamilyForm
 
 # Create your views here.
 def family_list(request):
     # families = Family.objects.all()
     # return render(request, 'family/family_list.html', {'families': families})
     return render(request, 'family/family_list.html', {'families': [0, 1, 2]})
+
+def family_create(request):
+    if request.method == 'POST':
+        form = FamilyForm(request.POST)
+        if form.is_valid():
+            # form.save()
+            return redirect('family:family_list')
+    else:
+        form = FamilyForm()
+    return render(request, 'family/family_create.html', {'form': form})
